c1e60cd27a86751f40b0b4ba139e3eb28f73ab99
sharmamayank-sketch <sharmamayank7501@gmail.com>
2025-11-06 11:00:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>React Dockerized App</h1>
+      <p>This React app is running inside a Docker container using Nginx.</p>
+    </div>
+  );
+}
+
+export default App;
